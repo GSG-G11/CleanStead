46864b3e9e1a8caa99cca18054cfb89a7b17a164
israaHamdi <israaaburayya2012@gmail.com>
2022-04-27 16:19:24 +0200

    setup server on port 5000

--- server/app.ts
+++ server/app.ts
@@ -1,3 +1,26 @@
-const express, {Application} = require('express');
+import { join } from 'path';
+import express, { Request, Response, Application } from 'express';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+import compression from 'compression';
 
 const app:Application = express();
+dotenv.config();
+app.set('port', process.env.PORT || 5000);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(compression());
+
+app.get('/', (req:Request, res:Response):void => {
+  res.json({ msg: 'Hello World!' });
+});
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(join(__dirname, '..', 'client', 'build')));
+  app.get('*', (req, res) => {
+    res.sendFile(join(__dirname, '..', 'client', 'build', 'index.html'));
+  });
+}
+
+export default app;
