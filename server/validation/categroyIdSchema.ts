615d9f26984245ec50cf92c8e91fe376f20375d5
Muhammad Abdulhadi <mu7ammadabed@gmail.com>
2022-05-01 08:37:38 +0300

    Update server/validation/categroyIdSchema.ts

--- server/validation/categroyIdSchema.ts
+++ server/validation/categroyIdSchema.ts
@@ -3,7 +3,7 @@ import {
 } from 'yup';
 
 const categroyIdSchema = object({
-  id: number().min(0).required('Category Id must be a number larger than 0'),
+  id: number().min(1).required('Category Id must be a number larger than 0'),
 });
 
 export default categroyIdSchema;
