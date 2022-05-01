2f4df150c352d26610a71e3a34280114c68537e7
MostafaQanoo <mostafaqanoo403@gmail.com>
2022-05-01 15:26:47 +0300

    edit

--- client/src/Components/WhyUs/index.jsx
+++ client/src/Components/WhyUs/index.jsx
@@ -1,46 +1,61 @@
 import React from 'react';
 import 'antd/dist/antd.css';
-import {
-  Comment, Avatar, Image, Row, Col,
-} from 'antd';
-import img from '../../assets/images/img.png';
-import logo1 from '../../assets/images/logo1.png';
-import logo2 from '../../assets/images/logo2.png';
-import logo3 from '../../assets/images/logo3.png';
-import logo4 from '../../assets/images/logo4.png';
+import { Comment, Avatar, Image, Row, Col, Typography } from 'antd';
+import img from '../../Assets/images/img.png';
+import logo1 from '../../Assets/images/logo1.png';
+import logo2 from '../../Assets/images/logo2.png';
+import logo3 from '../../Assets/images/logo3.png';
+import logo4 from '../../Assets/images/logo4.png';
 import './WhyUs.css';
 
-function Demo() {
+const { Title } = Typography;
+function WhyUs() {
   return (
     <div>
       <Row>
         <Col span={12}>
           <div className="section">
             <Comment
-              author={<a className="title">الموثوقية</a>}
+              author={<Title className="title">الموثوقية</Title>}
               avatar={<Avatar className="avatar" src={logo4} alt="Han Solo" />}
-              content={<p className="description">ندرك صعوبة القيام بتنظيف السجاد من قبل النساء</p>}
+              content={
+                <p className="description">
+                  ندرك صعوبة القيام بتنظيف السجاد من قبل النساء
+                </p>
+              }
             />
           </div>
           <div className="section">
             <Comment
-              author={<a className="title">الحجز اونلاين</a>}
+              author={<Title className="title">الحجز اونلاين</Title>}
               avatar={<Avatar className="avatar" src={logo3} alt="Han Solo" />}
-              content={<p className="description">ندرك صعوبة القيام بتنظيف السجاد من قبل النساء</p>}
+              content={
+                <p className="description">
+                  ندرك صعوبة القيام بتنظيف السجاد من قبل النساء
+                </p>
+              }
             />
           </div>
           <div className="section">
             <Comment
-              author={<a className="title">خصومات دائمة</a>}
+              author={<Title className="title">خصومات دائمة</Title>}
               avatar={<Avatar className="avatar" src={logo2} alt="Han Solo" />}
-              content={<p className="description">ندرك صعوبة القيام بتنظيف السجاد من قبل النساء</p>}
+              content={
+                <p className="description">
+                  ندرك صعوبة القيام بتنظيف السجاد من قبل النساء
+                </p>
+              }
             />
           </div>
           <div className="section">
             <Comment
-              author={<a className="title">دعم متواصل</a>}
+              author={<Title className="title">دعم متواصل</Title>}
               avatar={<Avatar className="avatar" src={logo1} alt="Han Solo" />}
-              content={<p className="description">ندرك صعوبة القيام بتنظيف السجاد من قبل النساء</p>}
+              content={
+                <p className="description">
+                  ندرك صعوبة القيام بتنظيف السجاد من قبل النساء
+                </p>
+              }
             />
           </div>
         </Col>
@@ -54,4 +69,4 @@ function Demo() {
   );
 }
 
-export default Demo;
+export default WhyUs;
--- client/src/Pages/Home/index.jsx
+++ client/src/Pages/Home/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { CategoriesCard, Header } from '../../Components';
+import { CategoriesCard, Header, WhyUs } from '../../Components';
 
 function Home() {
   return (
@@ -11,6 +11,7 @@ function Home() {
         imageUrl="https://media.istockphoto.com/photos/the-countdown-to-clean-shiny-floors-picture-id1291180143?b=1&k=20&m=1291180143&s=170667a&w=0&h=Fc66R5Pn_Gs2K1XSKrwqX49aPmVwqINbY_oKqBh5IRo="
         description="يجب علينا دائما أن نتواجد في مكان نظيف ومرتب لذلك نتوقع دائما أن يكون منزلك بهذا الشكل ونتفهم أيضًا أنه قد لا يكون لديك دائمًا الوقت للقيام بذلك بنفسك أو قد لاتكون لديك الصحة للتنظيف. لذلك لدينا فريق مهني لضمان حصولك على منزل نظيف بما يرضيك. ما عليك سوى التقديم للاستفادة من خدماتنا"
       />
+      <WhyUs />
     </div>
   );
 }
