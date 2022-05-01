cd783ef9f1ff39d3d05212b8bb1dcb2c404cbee6
MostafaQanoo <mostafaqanoo403@gmail.com>
2022-05-01 15:51:23 +0300

    edit style

--- client/src/Pages/Home/index.jsx
+++ client/src/Pages/Home/index.jsx
@@ -5,13 +5,13 @@ function Home() {
   return (
     <div>
       <Header />
+      <WhyUs />
       <CategoriesCard
         categoryId={1}
         title="تنظيف المنازل"
         imageUrl="https://media.istockphoto.com/photos/the-countdown-to-clean-shiny-floors-picture-id1291180143?b=1&k=20&m=1291180143&s=170667a&w=0&h=Fc66R5Pn_Gs2K1XSKrwqX49aPmVwqINbY_oKqBh5IRo="
         description="يجب علينا دائما أن نتواجد في مكان نظيف ومرتب لذلك نتوقع دائما أن يكون منزلك بهذا الشكل ونتفهم أيضًا أنه قد لا يكون لديك دائمًا الوقت للقيام بذلك بنفسك أو قد لاتكون لديك الصحة للتنظيف. لذلك لدينا فريق مهني لضمان حصولك على منزل نظيف بما يرضيك. ما عليك سوى التقديم للاستفادة من خدماتنا"
       />
-      <WhyUs />
     </div>
   );
 }
