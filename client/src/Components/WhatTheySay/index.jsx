763076cc3271d1b1f9af36b777550f9eda35886e
MohammadAlHabil <mohammadalhabil1999@gmail.com>
2022-05-07 16:10:45 +0300

    add what they say section relates #23 #28

---
+++ client/src/Components/WhatTheySay/index.jsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import { Grid, Row, Col } from 'antd';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination } from 'swiper';
+import QuoteCard from '../QuoteCard';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+import './style.css';
+
+const { useBreakpoint } = Grid;
+
+function WhatTheySay() {
+  const screens = useBreakpoint();
+  let count = 3;
+  if (screens.lg) count = 3;
+  else if (screens.md) count = 2;
+  else if (screens.sm) count = 2;
+  else if (screens.xl) count = 1;
+  else if (screens.xs) count = 1;
+
+  const quotes = [
+    {
+      name: 'محمد',
+      desc: 'دائما يقومون بعمل جيد وبالوقت المحدد, انا حقا سعيد بهذه التجربة',
+      img: 'https://picsum.photos/200/300',
+    },
+    {
+      name: 'اسراء',
+      desc: 'دائما يقومون بعمل جيد وبالوقت المحدد, انا حقا سعيد بهذه التجربة',
+      img: 'https://picsum.photos/200/300',
+    },
+    {
+      name: 'جهاد',
+      desc: 'دائما يقومون بعمل جيد وبالوقت المحدد, انا حقا سعيد بهذه التجربة',
+      img: 'https://picsum.photos/200/300',
+    },
+    {
+      name: 'مصطفى',
+      desc: 'دائما يقومون بعمل جيد وبالوقت المحدد, انا حقا سعيد بهذه التجربة',
+      img: 'https://picsum.photos/200/300',
+    },
+  ];
+
+  return (
+    <Row align="middle" justify="center">
+      <Col xs={{ span: 23 }}>
+        <Swiper
+          slidesPerView={count}
+          spaceBetween={30}
+          pagination={{
+            clickable: true,
+          }}
+          modules={[Pagination]}
+          className="mySwiper"
+        >
+          {quotes.length &&
+            quotes.map((quote) => (
+              <SwiperSlide key={Math.random()}>
+                <QuoteCard quote={quote} />
+              </SwiperSlide>
+            ))}
+        </Swiper>
+      </Col>
+    </Row>
+  );
+}
+
+export default WhatTheySay;
