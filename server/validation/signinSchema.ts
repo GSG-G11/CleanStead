f4aa1288f57be90eef795171ab69bb0b947fdea0
israaHamdi <israaaburayya2012@gmail.com>
2022-05-09 12:37:38 +0200

    edit message for validation

--- server/validation/signinSchema.ts
+++ server/validation/signinSchema.ts
@@ -4,7 +4,7 @@ import {
 
 const signinSchema = object({
   email: string().email('Your email must be a valid email').required('Your email is required to login'),
-  password: string().length(10, 'Your password must be 6 digits at least').required('Password is required to login'),
+  password: string().length(10, 'Your password must be 10 digits at least').required('Password is required to login'),
 });
 
 export default signinSchema;
