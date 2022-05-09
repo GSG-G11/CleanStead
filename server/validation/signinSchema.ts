3619bffb6fe6daca9be96e3a7ff74b2ee9c0339a
israaHamdi <israaaburayya2012@gmail.com>
2022-05-09 12:05:43 +0200

    add sign in schema for validation

---
+++ server/validation/signinSchema.ts
@@ -0,0 +1,11 @@
+import {
+    number, object, string,
+  } from 'yup';
+  
+  const signinSchema = object({
+    email: string().email('Your email must be a valid email').required('Your email is required to login'),
+    password: string().min(6, 'Your password must be 6 digits at least').required('Password is required to login'),
+  });
+  
+  export default signinSchema;
+  
