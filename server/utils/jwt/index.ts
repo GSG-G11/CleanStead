30a5deeba06cdf42bdc9488a56c1859d37cb4ea8
israaHamdi <israaaburayya2012@gmail.com>
2022-05-10 16:08:27 +0200

    convert sign to promise

--- server/utils/jwt/index.ts
+++ server/utils/jwt/index.ts
@@ -1,1 +1,2 @@
 export { default as jwtVerify } from './jwtVerify';
+export { default as jwtSign } from './jwtSign';
--- server/utils/jwt/jwtVerify.ts
+++ server/utils/jwt/jwtVerify.ts
@@ -3,7 +3,7 @@ import { verify } from 'jsonwebtoken';
 
 dotenv.config();
 const jwtVerify = (token:string) => new Promise((resolve, reject) => {
-  verify(token, process.env.JJWT_SECRET as string, (err:any, decoded:any) => {
+  verify(token, process.env.SECRET_KEY as string, (err:any, decoded:any) => {
     if (err) {
       reject(err);
     } else {
