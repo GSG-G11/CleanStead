d42d022623770c8f0fd7d05e11b9e6f3e1680ed8
MohammadAlHabil <mohammadalhabil1999@gmail.com>
2022-05-11 03:16:16 +0300

    edit the query of get books

--- server/queries/book/getBooksQuery.ts
+++ server/queries/book/getBooksQuery.ts
@@ -1,4 +1,17 @@
 import connection from '../../database/connection';
 
-const getBooksQuery = () => connection.query('SELECT * FROM appoinments');
+const getBooksQuery = () => connection.query(`
+  SELECT
+    a.id,
+    a.date,
+    a.time,
+    a.price,
+    a.status,
+    a.repeat,
+    a.user_id,
+    u.name,
+    u.location,
+  FROM
+    appoinments AS a
+  JOIN  users AS u ON  a.user_id = u.id`);
 export default getBooksQuery;
