8e57d2d00434b94998738029234c1a28773b9dc8
Jehad91 <j.abushaqra1@gmail.com>
2022-05-14 16:34:50 +0300

    delete console and comments

--- client/src/Components/BookNow/BookContainer.jsx
+++ client/src/Components/BookNow/BookContainer.jsx
@@ -14,11 +14,9 @@ const { Step } = Steps;
 function BookContainer({ categories }) {
   const [current, setCurrent] = useState(0);
   const [checked, setChecked] = useState([]);
-  const [isCheck, setIsCheck] = useState(false);
 
   const onCheck = (event) => {
     const isChecked = event.target.checked;
-    setIsCheck(isChecked);
     if (isChecked) {
       setChecked([...checked, event.target.value]);
     } else {
@@ -95,7 +93,7 @@ function BookContainer({ categories }) {
           lg={{ span: 7 }}
           xl={{ span: 7 }}
         >
-          <Summary checked={checked} isCheck={isCheck} />
+          <Summary checked={checked} />
         </Col>
       </Row>
     </div>
