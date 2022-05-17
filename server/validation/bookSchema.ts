00045e4b6ec763d83635b39a0436f0b9aab1013b
Jehad91 <j.abushaqra1@gmail.com>
2022-05-17 16:44:14 +0300

    add quantity insted of id

--- server/validation/bookSchema.ts
+++ server/validation/bookSchema.ts
@@ -8,7 +8,7 @@ const categoriesSchema = object({
   price: number().min(1, 'Price must be large than 0').required('Price is required'),
   repeat: string().required('Repeat Time is required'),
   userId: number().min(1, 'Id must be a positive number').required('User Id is required'),
-  quantity: number().min(1, 'Id must be a positive number').required('Quantity is required'),
+  quantity: number().min(1, 'Quantity must be a positive number').required('Quantity is required'),
   serviceId: number().min(1, 'Id must be a positive number').required('Service Id is required'),
 });
 
